d1008c9352f53c96d8d1b074988e7252c3d16e92
RAPL220 <rahmadaditya220@gmail.com>
2025-07-28 09:04:50 +0700

    Add admin/logout.php 1753668289269

---
+++ admin/logout.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Admin Logout
+ * Sistem Manajemen Surat
+ */
+
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../includes/auth.php';
+
+// Logout user
+logoutUser();
+
+// Redirect to login page
+header('Location: login.php');
+exit;
+?>
